1f8f1a9fea3d4658b3b72597d5e224c109f0fcb6
TomasDiazV95 <tignacio123@gmail.com>
2023-10-13 15:02:33 -0300

    se crea funcion basica mas html

---
+++ script.js
@@ -0,0 +1,24 @@
+function convertirCelciusAFarenheit(celsius) {
+    return (celsius * 1.8) + 32;
+}
+
+function convertirCelciusAKelvin(celsius) {
+    return celsius + 273.15;
+}
+
+function convertirTemperatura() {
+    let grados = document.getElementById('inputCelsius').value;
+
+    const celsius = parseFloat(grados);
+    const fahrenheit = convertirCelciusAFarenheit(celsius);
+    const kelvin = convertirCelciusAKelvin(celsius);
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerText = `Grados fahrenheit: ${fahrenheit}`;
+
+    const resultado2 = document.getElementById('resultado2');
+    resultado2.innerText = `Grados kelvin: ${kelvin}`;
+    
+}
+
+convertirTemperatura()
